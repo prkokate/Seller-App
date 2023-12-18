f260e54e090ef421915888c0029098aec7b9597a
Prathamesh Kokate <132193868+prkokate@users.noreply.github.com>
2023-12-18 18:22:19 +0530

    deployment 4

--- client/src/context/users/UserState.js
+++ client/src/context/users/UserState.js
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 const UserState=(props)=>{
 
 
-    const host="https://seller-app-server.vercel.app";
+    // const host="https://seller-app-server.vercel.app";
+    const host="http://localhost:8000"
     const [token,settoken]= useState(localStorage.getItem('token')) ;
     const initcar=[]
     const [cars, setcars]=useState(initcar);
